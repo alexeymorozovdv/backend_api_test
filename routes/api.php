172f26336c524deee4efc7b2f580358c97264bb4
Alexey Morozov <amorozov@simtechdev.org>
2023-03-18 17:25:23 +0300

    Removed unnecessary group from routes, adds auth:sanctum middleware to one route

--- routes/api.php
+++ routes/api.php
@@ -22,9 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
-
-Route::middleware('auth:sanctum')->group( function () {
-    Route::get('products', ProductController::class);
-});
-
-
+Route::get('products', ProductController::class)->middleware('auth:sanctum');
