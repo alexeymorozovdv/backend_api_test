d350fc9dafd2b381447541f4daddcba73c2a4efa
Alexey Morozov <amorozov@simtechdev.org>
2023-03-18 17:15:04 +0300

    Create migrations

---
+++ database/migrations/2023_03_18_055938_create_product_property_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Property;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_property', function (Blueprint $table) {
+            $table->foreignIdFor(Product::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Property::class)->constrained()->onDelete('cascade');
+
+            $table->index(['product_id', 'property_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_option_pivot');
+    }
+};
---
+++ database/migrations/2023_03_18_112946_adds_api_token_and_phone_number_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone_number', 12)->unique()->after('name');
+            $table->string('api_token', 60)->unique()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['phone_number']);
+            $table->dropColumn(['api_token']);
+        });
+    }
+};
